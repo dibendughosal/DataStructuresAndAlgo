0aab5a0e9acf14fd76c13654c856c610c2f0952d
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2025-04-08 23:45:27 +0530

    Array - Creation Done

---
+++ Array/Array.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int marks[5] = { 78, 93, 84, 74, 94};
+    cout<<marks[0]<<endl;
+    cout<<marks[1]<<endl;
+    cout<<marks[2]<<endl;
+    cout<<marks[3]<<endl;
+    cout<<marks[4]<<endl;
+}
