24242cfd92a3b780278cdba1714dcbcded675d7e
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2025-08-28 21:14:35 +0530

    Leetcode 169 Solved

--- Sorting/insertionSort.cpp
+++ Sorting/insertionSort.cpp
@@ -6,14 +6,12 @@ void insertionSort(int arr[], int n){
     for (int i = 1; i < n; i++)
     {
         int j = i;
-        while( j > 0){
-            if(arr[j]>arr[j-1]) break;
-            else{
+        while( j > 0 && arr[j] < arr[j-1]){
                 int temp = arr[j-1];
                 arr[j-1]= arr[j];
                 arr[j] =  temp;
                 j--;
-            }
+            
         }
     }
     
